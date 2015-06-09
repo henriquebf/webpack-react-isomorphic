3b5c6b790ddb7f87676eac7bf0005b1c7743a7fc
Henrique Ferreira <henriquebf@gmail.com>
2015-06-09 17:57:50 +0200

    Remove vendors

--- gulp/tasks/copyJsVendor.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var config = require('../config').copyJsVendor;
-
-gulp.task('copyJsVendor', function() {
-
-    return gulp.src(config.srcDir)
-        .pipe(gulp.dest(config.destDir));
-});
