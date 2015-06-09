b0b331102b7a72402bbf489bd27852eee95423d8
Henrique Ferreira <henriquebf@gmail.com>
2015-06-09 17:58:11 +0200

    Add intro component

---
+++ app/pages/App/screens/Template/screens/Home/components/Intro.js
@@ -0,0 +1,21 @@
+/**
+ * @name Intro
+ * @component
+ *
+ * @description
+ * - Intro Section
+ */
+
+import React from 'react';
+
+export default React.createClass({
+
+    render() {
+        return (
+            <div className="intro">
+                Intro
+            </div>
+        )
+    }
+
+});
