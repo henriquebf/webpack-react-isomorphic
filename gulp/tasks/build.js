812180e5ab0b9b5ec78d5713ab6608d444fe4385
Henrique Ferreira <henriquebf@gmail.com>
2015-06-09 18:00:11 +0200

    remove gulp

--- gulp/tasks/build.js
+++
@@ -1,8 +0,0 @@
-var gulp            = require('gulp');
-var runSequence     = require('run-sequence');
-
-gulp.task('build', function() {
-
-    runSequence('clean', ['webpack', 'copy', 'copyJsVendor']);
-
-});
